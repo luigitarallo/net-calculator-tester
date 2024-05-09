5d455bd2c3236a3a673a9fb72f5890189b2bfef1
luigitarallo <tarallo.gigi@gmail.com>
2024-05-09 15:23:37 +0200

    Add test with 0 divisor

--- TestCalculator/UnitTest1.cs
+++ TestCalculator/UnitTest1.cs
@@ -11,9 +11,9 @@ namespace TestCalculator
         [Test]
         public void TestAdd()
         {
-            float num1 = 5;
-            float num2 = 3;
-            float expected = 8;
+            float num1 = 5.5f;
+            float num2 = 3.2f;
+            float expected = 8.7f;
 
             float result = Calculator.Add(num1, num2);
 
@@ -23,9 +23,9 @@ namespace TestCalculator
         [Test]
         public void TestSubtract()
         {
-            float num1 = 5;
-            float num2 = 3;
-            float expected = 2;
+            float num1 = 55.5f;
+            float num2 = 33.3f;
+            float expected = 22.2f;
             float result = Calculator.Subtract(num1, num2);
 
             Assert.AreEqual(expected, result);
@@ -35,8 +35,8 @@ namespace TestCalculator
         public void TestDivide()
         {
             float num1 = 6;
-            float num2 = 2;
-            float expected = 3;
+            float num2 = 0;
+            float expected = 0;
 
             float result = Calculator.Divide(num1, num2);
 
@@ -46,9 +46,9 @@ namespace TestCalculator
         [Test]
         public void TestMultiply()
         {
-            float num1 = 5;
-            float num2 = 4;
-            float expected = 20;
+            float num1 = 5.5f;
+            float num2 = 1;
+            float expected = 5.5f;
 
             float result = Calculator.Multiply(num1, num2);
 
