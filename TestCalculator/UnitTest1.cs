860822f62496e2626a8a59c16e243ad4b9c9d4e0
luigitarallo <tarallo.gigi@gmail.com>
2024-05-09 17:34:01 +0200

    add more assert

--- TestCalculator/UnitTest1.cs
+++ TestCalculator/UnitTest1.cs
@@ -13,11 +13,10 @@ namespace TestCalculator
         {
             float num1 = 5.5f;
             float num2 = 3.2f;
-            float expected = 8.7f;
 
-            float result = Calculator.Add(num1, num2);
-
-            Assert.AreEqual(expected, result);
+            Assert.AreEqual(8.7f, Calculator.Add(num1, num2));
+            Assert.AreEqual(10.7f, Calculator.Add(num1, 5.2f));
+            Assert.AreEqual(-2f, Calculator.Add(-7.5f, 5.5f));
         }
 
         [Test]
@@ -25,34 +24,31 @@ namespace TestCalculator
         {
             float num1 = 55.5f;
             float num2 = 33.3f;
-            float expected = 22.2f;
-            float result = Calculator.Subtract(num1, num2);
 
-            Assert.AreEqual(expected, result);
+            Assert.AreEqual(22.2f, Calculator.Subtract(num1, num2));
+            Assert.AreEqual(20.3f, Calculator.Subtract(num1, 35.2f));
+            Assert.AreEqual(-5.5f, Calculator.Subtract(0, 5.5f));
         }
 
         [Test]
         public void TestDivide()
         {
             float num1 = 6;
-            float num2 = 0;
-            float expected = 0;
+            float num2 = 3;
 
-            float result = Calculator.Divide(num1, num2);
-
-            Assert.AreEqual(expected, result);
+            Assert.AreEqual(2, Calculator.Divide(num1, num2));
+            Assert.AreEqual(0.5f, Calculator.Divide(num2, num1));
+            Assert.AreEqual(3, Calculator.Divide(num1, 2));
         }
 
         [Test]
         public void TestMultiply()
         {
             float num1 = 5.5f;
-            float num2 = 1;
-            float expected = 5.5f;
+            float num2 = 2;
 
-            float result = Calculator.Multiply(num1, num2);
-
-            Assert.AreEqual(expected, result);
+            Assert.AreEqual(11, Calculator.Multiply(num1, num2));
+            Assert.AreEqual(16.5f, Calculator.Multiply(num1, 3));
         }
     }
 }
